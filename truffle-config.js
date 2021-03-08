07983f21ad4fe24fc166c8efe4d686f9ea98eb27
breeze <ts001_breeze@space-p.com>
2021-03-08 16:07:03 +0800

    add merkleDistributor migrate

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,25 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const result = require('dotenv').config(); // read .env file saved in project root
+if (result.error) {
+  throw result.error;
+}
+
+const hecoChainProvider = (network) => {
+  let rpcs = {
+    'mainnet': 'https://http-mainnet.hecochain.com',
+    'testnet': 'https://http-testnet.hecochain.com'
+  }
+  return new HDWalletProvider(process.env.DEPLOYER_PRIVATE_KEY || '', rpcs[network])
+};
+
+const bnbChainProvider = (network) => {
+  let rpcs = {
+    'mainnet': 'https://bsc-dataseed.binance.org',
+    'testnet': 'https://data-seed-prebsc-1-s1.binance.org:8545'
+  }
+  return new HDWalletProvider(process.env.DEPLOYER_PRIVATE_KEY || '', rpcs[network])
+};
+
 module.exports = {
   // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
@@ -5,28 +27,56 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
+
+  // Modify to the correct migration directory when using
+  // migrations_directory: "./migrations/ignore_migrations",
+  migrations_directory: "./migrations/",
+  networks: {
+    hecomainnet:{
+      provider: hecoChainProvider('mainnet'),
+      network_id: "*",  // match any network
+      gas: 6721975,
+      networkCheckTimeout: 60000,
+    },
+    development: {
+      host: "8.129.187.233",
+      port: 28545,
+      network_id: "*"
+    },
+    local: {
+      host: '127.0.0.1',
+      port: 8545,
+      // gasPrice: 100000000000, // 100 gwei
+      gas: 6721975,
+      network_id: '*',
+    },
+    hecotestnet:{
+      provider: hecoChainProvider('testnet'),
+      network_id: "256",  // match any network
+      gas: 6721975,
+      networkCheckTimeout: 60000,
+    },
+    bnbtestnet: {
+      provider: bnbChainProvider('testnet'),
+      network_id: "97",  // match any network
+      gas: 6721975,
+      networkCheckTimeout: 60000,
+    },
+    bnbmainnet: {
+      provider: bnbChainProvider('mainnet'),
+      network_id: "56",  // match any network
+      gas: 6721975,
+      networkCheckTimeout: 60000,
+    }
+  },
   //
   compilers: {
     solc: {
-      version: "v0.6.12+commit.27d51765"
-    }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
+      version: "0.6.12",
+      "optimizer": {
+        "enabled": true,
+        "runs": 200
+      }
     }
   }
 };
